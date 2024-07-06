95c808b79689ffbc33cb3566205bee3f0f6f42cf
Vincent Gates <vincentgates@outlook.com>
2024-07-05 22:13:37 -0500

    Routes placeholder refacted to dectionary

--- app/controllers/pages.py
+++ app/controllers/pages.py
@@ -3,36 +3,35 @@ from app.forms import *
 
 blueprint = Blueprint('pages', __name__)
 
+# Dictionary mapping routes to their corresponding placeholder templates
+app_placeholder = {
+    "/": "home",
+    "/features": "features",
+    "/cal": "calendar",
+    "/faq": "faq",
+}
 
-################
-#### routes ####
-################
+# Helper function to create a route function with a unique endpoint
+def create_route(template_path):
+    def route():
+        return render_template(template_path)
+    return route
 
+# Routes created dynamically looping through the dictionary
+for route, name in app_placeholder.items():
+    template_path = f'pages/placeholder.{name}.html'
+    blueprint.add_url_rule(route, endpoint=f"pages_{name}", view_func=create_route(template_path))
 
-@blueprint.route('/')
-def home():
-    return render_template('pages/placeholder.home.html')
-
-
-@blueprint.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-@blueprint.route('/cal')
-def calendar():
-    return render_template('pages/calendar.html')
-
+# Routes with forms
 @blueprint.route('/login')
 def login():
     form = LoginForm(request.form)
     return render_template('forms/login.html', form=form)
 
-
 @blueprint.route('/register')
 def register():
     form = RegisterForm(request.form)
     return render_template('forms/register.html', form=form)
-
 
 @blueprint.route('/forgot')
 def forgot():
