d197b7ec511a4fad5769f49690b0263132f47fee
Vincent Gates <vincentgates@outlook.com>
2024-07-08 13:49:23 -0500

    initial working copy of watch_scss.py

---
+++ config/development/settings.py
@@ -0,0 +1,9 @@
+import os
+
+# Basic Flask settings
+DEBUG = True
+TESTING = False
+SECRET_KEY = '1a3a5858d7695287ef65558467b24bf15cb19138c01f8d09'  # Replace with the generated key
+
+# Flask-Assets settings
+ASSETS_DEBUG = True
